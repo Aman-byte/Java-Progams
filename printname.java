497cbafa9b0b01f41cccc56d7b7e6bce4bfb899f
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-22 23:40:39 +0530

    Create printname.java

---
+++ printname.java
@@ -0,0 +1,9 @@
+//printing hello in one line and then name in the next line.
+public class Main
+{
+    public static void main(String[] args) {
+        String name = "john";
+        System.out.println("hello\n" + name);
+        
+    }
+}
