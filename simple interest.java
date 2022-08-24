dfd84847a923fb666184260f63d614781dd0d26e
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-24 16:54:17 +0530

    Create simple interest.java

---
+++ simple interest.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int principal = 1000;
+        int rate = 5;
+        int time =2;
+        int simpleinterest = (principal * rate *time)/100; //formula
+        System.out.println("the simple interest is " +simpleinterest);
+    }
+}
