3837e6885cf197473197c384f11b69e4bd8275e6
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-25 20:47:44 +0530

    Create fibonnaci.java

---
+++ fibonnaci.java
@@ -0,0 +1,55 @@
+class fibonnaci {
+  public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
+
+/* using while loop
+class fibonnaci {
+  public static void main(String[] args) {
+
+    int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    while (i <= n) {
+      System.out.print(firstTerm + ", ");
+
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+
+      i++;
+    }
+  }
+}
+
+Display Fibonacci series up to a given number
+class Fibonacci {
+  public static void main(String[] args) {
+
+    int n = 100, firstTerm = 0, secondTerm = 1;
+        
+    System.out.println("Fibonacci Series Upto " + n + ": ");
+    
+    while (firstTerm <= n) {
+      System.out.print(firstTerm + ", ");
+
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+
+    }
+  }
+}
+*/
