814fc526e0f544638df1e577f4de123fb0916199
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-25 16:28:26 +0530

    Update whileLoop.java

--- whileLoop.java
+++ whileLoop.java
@@ -6,8 +6,9 @@ public class whileLoop {
         int counter = 1;
         while(counter <=10)
         {
-            System.out.println("1 will always be less than 10 hence printing an infinite loop using while loop");
-            counter = counter +1; //acts as a break
+            System.out.println("Since 1 will always be less than 10 hence printing an infinite loop using while loop");
+            System.out.println(counter); //how many times it will print the above statement will print
+            counter = counter +1; //breaking condition
         }
     }
 }
