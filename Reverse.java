8f3ed29690689234c2942504e9e39e04b6748b19
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-26 20:00:05 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class Reverse {
+public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Enter the number to be reversed");
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        int ans = 0;
 
         while (n!=0) {
             int rem = n % 10;
-            int ans = ans * 10 + rem;
+            ans = ans * 10 + rem;
             n = n / 10;
         }
-        System.out.println("Enter the number to be reversed");
         System.out.println(ans);
 
     }
