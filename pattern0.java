e0aeabd698571983e21bfd4c3fffb30dcd8047ec
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-27 13:21:14 +0530

    Create pattern0.java

---
+++ pattern0.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter no of rows");
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int row =1; row <=n; row ++){ //row increase by 1
+
+            System.out.println("*");
+
+
+        }
+    }
+}
