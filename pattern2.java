db53580178339fe656d36ec6312b0e4e5ae746b6
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2022-08-28 11:20:56 +0530

    Create pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int nst = 1;
+
+        int row = 1;
+        while(row <= n) {
+
+            int cst = 1;
+            while(cst <= nst) {
+                System.out.print("*");
+                cst++;
+            }
+            System.out.println();
+            nst=nst+1;
+            row=row+1;
+        }
+    }
+}
